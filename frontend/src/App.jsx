4099eeeda5275b762fe482fe632fd6ce800fcbcf
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-24 03:05:52 +0530

    add Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import Navigation from './pages/Auth/Navigation'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <ToastContainer />
+    <Navigation />
+    <main className='py-3'>
+      <Outlet />
+    </main>
+    </>
   )
 }
 
