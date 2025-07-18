e543cf3889962c0fc714bd43542e0d95cf3dafc3
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-18 09:54:22 +0530

    frontend for production

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,8 @@
-// export const BASE_URL = 'https://test-portal-79im.onrender.com' // For production, uncomment this line and comment the above line
+const API = import.meta.env.BACKEND_URL;
 
-export const BASE_URL = ''
+export const BASE_URL = API // For production, uncomment this line and comment the above line
+
+// export const BASE_URL = ''
 export const USERS_URL = '/api/users' 
 export const CATEGORY_URL= '/api/category'
 export const PRODUCT_URL = '/api/product'
