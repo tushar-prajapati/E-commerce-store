0f25575c7a9eef81a99d5ca6929d58c9ebcb869a
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-18 10:05:16 +0530

    stablizing frontend

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,6 @@
-const API = import.meta.env.BACKEND_URL;
+// const API = import.meta.env.BACKEND_URL;
 
-export const BASE_URL = API // For production, uncomment this line and comment the above line
+export const BASE_URL = 'https://e-commerce-store-6z26.onrender.com' // For production, uncomment this line and comment the above line
 
 // export const BASE_URL = ''
 export const USERS_URL = '/api/users' 
