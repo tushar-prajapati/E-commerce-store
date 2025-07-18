deb44becad8f2d177288223d78b32952b26802b1
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-18 12:56:29 +0530

    add eye

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,87 +1,125 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import {Link,  useLocation, useNavigate} from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { setCredentials } from '../../redux/features/auth/authSlice.js'
-import { useLoginMutation } from '../../redux/api/usersApiSlice.js'
-import { toast } from 'react-toastify'
-import Loader from '../../components/Loader.jsx'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setCredentials } from "../../redux/features/auth/authSlice.js";
+import { useLoginMutation } from "../../redux/api/usersApiSlice.js";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader.jsx";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState('');
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [login, {isLoading}] = useLoginMutation()
-    const {userInfo} = useSelector(state=>state.auth)
-    const {search} = useLocation();
-    const sp= new URLSearchParams(search)
-    const redirect = sp.get('redirect') || '/'
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  const [showPassword, setShowPassword] = useState(false);
 
-    useEffect(()=>{
-        if(userInfo){
-            navigate(redirect);
-        }
-    },[navigate, redirect, userInfo])
-
-    const submitHandler = async(e)=>{
-        e.preventDefault();
-        try {
-            const res = await login({email, password}).unwrap();
-            console.log(res)
-            dispatch(setCredentials({...res}))
-            toast.success(`Welcome Back, ${res.username}`)
-        } catch (error) {
-            toast.error(error?.data?.message || error.message)
-        }
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
     }
+  }, [navigate, redirect, userInfo]);
 
-
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ email, password }).unwrap();
+      console.log(res);
+      dispatch(setCredentials({ ...res }));
+      toast.success(`Welcome Back, ${res.username}`);
+    } catch (error) {
+      toast.error(error?.data?.message || error.message);
+    }
+  };
 
   return (
     <div>
-        <section className='pl-[10rem] flex flex-wrap '>
-            <div className="mr-[4rem] mt-[5rem]">
-                <h1 className='text-2xl font-semibold mb-4'>Sign In</h1>
-                <form onSubmit={submitHandler} className='container w-[40rem]'>
-                    <div className='my-[2rem]'>
-                        <label htmlFor="email" className='block text-sm font-medium text-white'>Email Address</label>
-                        <input type="email" id="email" className='mt-1 p-2 border rounded w-full'
-                        value={email}
-                        onChange={e=>setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className='my-[2rem]'>
-                        <label htmlFor="email" className='block text-sm font-medium text-white'>Password</label>
-                        <input type="password" id="password" className='mt-1 p-2 border rounded w-full'
-                        value={password}
-                        onChange={e=>setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button disabled={isLoading} type='submit' className='bg-pink-500 text-white px-4 py-2 cursor-pointer rounded my-[1rem]'>{isLoading? "Signing In..": "Sign In"}</button>
-
-                    {isLoading && <Loader />}
-                </form>
-                <div className="mt-4">
-                    <p className="text-white">
-                        New Customer ? {" "}
-                        <Link to={redirect? `/register?redirect=${redirect}`: '/register'}
-                        className='text-pink-500 hover:underline'
-                        >Register</Link>
-                    </p>
-                </div>
+      <section className="pl-[10rem] flex flex-wrap ">
+        <div className="mr-[4rem] mt-[5rem]">
+          <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+          <form onSubmit={submitHandler} className="container w-[40rem]">
+            <div className="my-[2rem]">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-white"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 p-2 border rounded w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            
-        <div className='h-screen w-[43%] flex items-center justify-center'>
-        <img
-        src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
-        alt=""
-        className="h-[90%] w-full xl:block md:hidden sm:hidden rounded-lg"
-      />
-        </div>
-        </section>
-    </div>
-  )
-}
+            <div className="my-[2rem] relative w-full">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-white"
+              >
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                style={{
+                  position: "absolute",
+                  right: "10px",
+                  top: "70%",
+                  transform: "translateY(-50%)",
+                  cursor: "pointer",
+                  color: "#fff",
+                }}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-pink-500 text-white px-4 py-2 cursor-pointer rounded my-[1rem]"
+            >
+              {isLoading ? "Signing In.." : "Sign In"}
+            </button>
 
-export default Login
+            {isLoading && <Loader />}
+          </form>
+          <div className="mt-4">
+            <p className="text-white">
+              New Customer ?{" "}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+                className="text-pink-500 hover:underline"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        <div className="h-screen w-[43%] flex items-center justify-center">
+          <img
+            src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+            alt=""
+            className="h-[90%] w-full xl:block md:hidden sm:hidden rounded-lg"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
